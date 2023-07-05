c6041b1598c8513bc98d10c624f65b23d1252766
Neal Kotval <nk04655@ad.georgiasouthern.edu>
2023-07-05 13:01:45 -0400

    sequential works but is capped

--- lung_cancer.py
+++ lung_cancer.py
@@ -1,40 +1,73 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('lung_cancer.csv')
+# Load the dataset
+data = pd.read_csv('lung_cancer.csv')
 
-# One-hot encode the gender column
-df_encoded = pd.get_dummies(df, columns=['gender'])
+# Separate features (X) and target variable (y)
+X = data.iloc[:, :-1]  # First 15 columns
+y = data.iloc[:, -1]   # Last column
 
-# Split the data into input (X) and output (y) variables
-X = df_encoded.drop('column_name', axis=1)  # Replace 'column_name' with the appropriate output column name
-y = df_encoded['column_name']  # Replace 'column_name' with the appropriate output column name
+# Encode categorical variables
+label_encoder = LabelEncoder()
+X_encoded = X.apply(label_encoder.fit_transform)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Perform one-hot encoding
+X_encoded = pd.get_dummies(X_encoded, drop_first=True)
 
-# Scale the input variables
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# Encode target variable
+y_encoded = label_encoder.fit_transform(y)
 
-# Build the deep neural network model
+# Split the data into training and testing sets with stratification
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y_encoded, test_size=0.2, stratify=y_encoded, random_state=42)
+
+# Convert the data to numpy arrays
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
 model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=X_train_scaled.shape[1]))
+model.add(Dense(512, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(4, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(2, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 
+
 # Compile the model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
 
-# Train the model
-model.fit(X_train_scaled, y_train, epochs=10, batch_size=32, verbose=1)
+# Train the model and collect history
+history = model.fit(X_train, y_train, epochs=500, batch_size=16, verbose=1)
 
-# Evaluate the model on the test set
-loss, accuracy = model.evaluate(X_test_scaled, y_test)
-print(f'Test Loss: {loss:.4f}')
-print(f'Test Accuracy: {accuracy:.4f}')
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy:", accuracy)
+
+# Plot the accuracy
+plt.plot(history.history['accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.show()
